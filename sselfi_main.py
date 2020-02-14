98aba981ce8eb8eed4999e5232f25a949b3e98cb
Francois <fr.eiffel@gmail.com>
2020-02-14 10:32:48 +0100

    Adds default nuber of params

--- sselfi_main.py
+++ sselfi_main.py
@@ -453,7 +453,7 @@ def main(unused_argv):
       params, FLAGS.config_file, is_strict=True)
   params = params_dict.override_params_dict(
       params, FLAGS.params_override, is_strict=True)
-
+  params['num_parameters'] = 2
   params = flags_to_params.override_params_from_input_flags(params, FLAGS)
 
   params.validate()
